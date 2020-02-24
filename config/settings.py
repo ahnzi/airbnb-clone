acc1840929e0c378cc4c8b4d43bbd73e0f6c0f7f
jihoon0621 <42331481+jihoon0621@users.noreply.github.com>
2020-02-24 12:29:40 +0900

    #4.1 Room Model part One
    #4.1 Room Model part One

--- config/settings.py
+++ config/settings.py
@@ -46,6 +46,8 @@ PROJECT_APPS = [
 
 INSTALLED_APPS = DJANGO_APPS + PROJECT_APPS
 
+THIRD_PARTY_APPS = ["django_countries"]
+
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
